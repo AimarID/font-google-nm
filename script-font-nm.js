250e0f900f8a69abfcdb8d69f4c26368eacb0ff7
AimarID <34244360+AimarID@users.noreply.github.com>
2018-06-17 09:48:31 +0700

    Add files via upload

---
+++ script-font-nm.js
@@ -0,0 +1,6 @@
+﻿<script type='text/javascript'>
+//<![CDATA[
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+loadCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css");loadCSS("https://fonts.googleapis.com/css?family=Open+Sans:400,700");loadCSS("https://fonts.googleapis.com/css?family=Roboto+Slab:400,700");
+//]]>
+</script>
